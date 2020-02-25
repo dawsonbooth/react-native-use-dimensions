6f604c9a1cc0182846d40d57c8ea6b710cf47a0a
dawsonbooth <github@dawsonbooth.com>
2020-02-25 13:50:55 -0600

    Replace tslint with eslint

--- tests/index.tsx
+++ tests/index.tsx
@@ -1,59 +1,59 @@
 import React from "react";
 import { Text } from "react-native";
 import useDimensions, {
-    useScreenDimensions,
-    useWindowDimensions
+  useScreenDimensions,
+  useWindowDimensions
 } from "../src";
 
 import renderer from "react-test-renderer";
 
 const ScreenDimensions: React.FC = () => {
-    const { height, width } = useScreenDimensions();
-    const isLandscape = width > height;
-    return (
-        <Text>
-            {width}x{height}
-            Orientation: {isLandscape ? "Landscape" : "Portrait"}
-        </Text>
-    );
+  const { height, width } = useScreenDimensions();
+  const isLandscape = width > height;
+  return (
+    <Text>
+      {width}x{height}
+      Orientation: {isLandscape ? "Landscape" : "Portrait"}
+    </Text>
+  );
 };
 
 const WindowDimensions: React.FC = () => {
-    const { height, width } = useWindowDimensions();
-    const isLandscape = width > height;
-    return (
-        <Text>
-            {width}x{height}
-            Orientation: {isLandscape ? "Landscape" : "Portrait"}
-        </Text>
-    );
+  const { height, width } = useWindowDimensions();
+  const isLandscape = width > height;
+  return (
+    <Text>
+      {width}x{height}
+      Orientation: {isLandscape ? "Landscape" : "Portrait"}
+    </Text>
+  );
 };
 
 const BothDimensions: React.FC = () => {
-    const { screen, window } = useDimensions();
-    return (
-        <Text>
-            Screen: {screen.width}x{screen.height}
-            Window: {window.width}x{window.height}
-        </Text>
-    );
+  const { screen, window } = useDimensions();
+  return (
+    <Text>
+      Screen: {screen.width}x{screen.height}
+      Window: {window.width}x{window.height}
+    </Text>
+  );
 };
 
 beforeAll(() => {
-    // For accurrate test times
-    const Fragment = () => (
-        <>
-            <ScreenDimensions />
-            <WindowDimensions />
-            <BothDimensions />
-        </>
-    );
-    renderer.create(<Fragment />).toJSON();
+  // For accurrate test times
+  const Fragment: React.FC = () => (
+    <>
+      <ScreenDimensions />
+      <WindowDimensions />
+      <BothDimensions />
+    </>
+  );
+  renderer.create(<Fragment />).toJSON();
 });
 
 test("Screen", () => {
-    const tree = renderer.create(<ScreenDimensions />).toJSON();
-    expect(tree).toMatchInlineSnapshot(`
+  const tree = renderer.create(<ScreenDimensions />).toJSON();
+  expect(tree).toMatchInlineSnapshot(`
         <Text>
           750
           x
@@ -65,8 +65,8 @@ test("Screen", () => {
 });
 
 test("Window", () => {
-    const tree = renderer.create(<WindowDimensions />).toJSON();
-    expect(tree).toMatchInlineSnapshot(`
+  const tree = renderer.create(<WindowDimensions />).toJSON();
+  expect(tree).toMatchInlineSnapshot(`
         <Text>
           750
           x
@@ -78,8 +78,8 @@ test("Window", () => {
 });
 
 test("Both", () => {
-    const tree = renderer.create(<BothDimensions />).toJSON();
-    expect(tree).toMatchInlineSnapshot(`
+  const tree = renderer.create(<BothDimensions />).toJSON();
+  expect(tree).toMatchInlineSnapshot(`
         <Text>
           Screen: 
           750
